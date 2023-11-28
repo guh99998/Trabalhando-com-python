a08199e176c607a9f87e9059c258bf21f7fee19f
Gustavo Lopes <guhrlopes999@gmail.com>
2023-11-28 20:15:45 -0300

    Alterado exemplo

--- 11/main.py
+++ 11/main.py
@@ -4,7 +4,7 @@ class App:
     @staticmethod
     def bem_vindo():
         print('---------------- Bem vindo ao Programa ----------------', end='\n\n')
-        
+
     def obter_numero_valido(self):
         while True:
             try:
@@ -15,24 +15,24 @@ class App:
                 continue
             else:
                 return numero
-            
+
     def exibir_numero(self, numero):
         print(f'\nO número que você digitou é {numero}', end='\n\n')
-        
+
     @staticmethod
     def final():
         print('---------------- Final do Programa ----------------', end='\n\n')
-        
+
 if __name__ == '__main__':
     os.system('cls')
-    
+
     app = App()
-    
+
     App.bem_vindo()
-    
+
     numero = app.obter_numero_valido()
-    
+
     app.exibir_numero(numero)
-    
+
 
     App.final()
